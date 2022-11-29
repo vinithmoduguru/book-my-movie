5ca6e387a3dc85aacd7a583d4b838a69ba0b1269
vinithmoduguru <vinith1142@gmail.com>
2022-11-29 22:57:13 +0530

    Added navigation bar

--- src/Components/Seats/seats.styles.jsx
+++ src/Components/Seats/seats.styles.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components"
 
 export const BookingContainer = styled.div`
-  /* font-size: 10px; */
   display: flex;
   flex-direction: column;
   align-items: center;
--- src/Routes/Navigation/navigation.component.jsx
+++ src/Routes/Navigation/navigation.component.jsx
@@ -1,8 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
+import {
+  LogoContainer,
+  NavigationContainer,
+  NavLinks,
+} from "./navigation.styles"
+
+import { ReactComponent as CrwnLogo } from "../../Assets/crown.svg"
 
 const Navigation = () => {
   return (
     <>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <CrwnLogo />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/">HOME</NavLink>
+        </NavLinks>
+      </NavigationContainer>
       <Outlet />
     </>
   )
